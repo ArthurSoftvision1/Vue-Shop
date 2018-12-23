62f73d2c512d3796b234499625f4c3b7f2be1ca3
MolnarArthur <arthur.molnar@usource.ro>
2018-12-23 22:55:28 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+ <div class="container">
+   <div class="row">
+     <div class="col-md-7">
+       <div class="row">
+         <div :key="product.id" class="col-md-6" v-for="product in products">
+           <product v-on:add-to-cart="addToCart(product)" :product="product"> </product>
+         </div>
+       </div>
+     </div>
+
+     <div class="col-md-5">
+
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
+
+
+export default {
+  name: 'app',
+  components: {
+    Product
+  },
+
+  data() {
+    return {
+      products,
+      cart: [],
+    }
+  },
+
+  methods: {
+    addToCart(product) {
+      this.cart.push(product)
+    }
+  }
+  
+}
+</script>
+
+<style>
+  body {
+    background-color: #FBF8F3;
+  }
+</style>
