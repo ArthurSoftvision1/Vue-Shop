5c594796d028f3c9305ec7edbea4a9feb12c00b0
MolnarArthur <arthur.molnar@usource.ro>
2018-12-24 11:41:23 +0200

    Remove item from cart

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@
      <div class="col-md-7">
        <div class="row">
          <div :key="product.id" class="col-md-6" v-for="product in products">
-           <product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"> </product>
+           <product 
+            :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"> 
+           </product>
          </div>
        </div>
      </div>
 
      <div class="col-md-5 my-5">
-       <cart :items="cart"></cart>
+       <cart v-on:remove-from-cart="removeFromCart($event)" :items="cart"></cart>
      </div>
    </div>
  </div>
@@ -49,6 +51,10 @@ export default {
       } else {
         return false
       }
+    },
+
+    removeFromCart(product) {
+      this.cart = this.cart.filter(item => item.id !== product.id)
     }
   }
 }
