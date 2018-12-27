6bd808e167920271924509486270da7c241fee44
MolnarArthur <arthur.molnar@usource.ro>
2018-12-27 18:46:52 +0200

    Save article

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -19,10 +19,13 @@
                             </option>
                         </select>
 
-                        <input type="text" placeholder="Title" class="form-control my-3">
+                        <input type="text" placeholder="Title" v-model="title" class="form-control my-3">
                         <wysiwyg v-model="content" />
                         <div class="text-center">
-                            <button @click="createArticle()" class="btn-success btn btn-lg mt-3">Create Article</button>
+                            <button :disabled="loading" @click="createArticle()" class="btn-success btn btn-lg mt-3">
+                                <i class="fas fa-spin fa-spinner" v-if="loading"></i>
+                                {{ loading ? '' : 'Create Category' }}
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -39,6 +42,14 @@ import config from '@/config';
 
 export default {
 
+    beforeRouteEnter(to, from, next) {
+        if (!localStorage.getItem('auth')) {
+            return next({ path: '/login' })
+        }
+
+        next();
+    },
+
     mounted() {
         this.getCategories();
     },
@@ -49,10 +60,12 @@ export default {
 
     data() {
         return {
+            title: "",
             content: "",
             image: null,
             categories: [],
-            category: ''
+            category: '',
+            loading: false
         }
     },
 
@@ -62,13 +75,41 @@ export default {
         },
 
         createArticle() {
+
+            if (!this.title || !this.image || !this.category || !this.content) {
+                this.$noty.error("Please fill in all fields.")
+                return;
+            }
+
+            this.loading = true
             const form = new FormData();
             form.append('file', this.image);
             form.append('upload_preset', process.env.VUE_APP_CLOUDINARY_PRESET)
             form.append('api_key', process.env.VUE_APP_CLOUDINARY_API_KEY)
 
             Axios.post(process.env.VUE_APP_CLOUDINARY_URL, form)
-                .then(res => console.log(res))
+                .then(res =>
+                    Axios.post(`${config.apiUrl}/articles`, {
+                        title: this.title,
+                        content: this.content,
+                        category_id: this.category,
+                        imageUrl: res.data.secure_url
+                    }, {
+                        headers: {
+                            Authorization: `Bearer ${this.$root.auth.token}`
+                        }
+                    }).then(() => {
+                        this.loading = false
+                        this.$noty.success('Article created successfully!');
+                        this.$router.push('/')
+                    }).catch(() => {
+                        this.loading = false
+                        this.$noty.error('Something went wrong!');
+                    })
+                ).catch(() => {
+                    this.loading = false
+                    this.$noty.error('Something went wrong!');
+                })
         },
 
         getCategories() {
